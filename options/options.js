ea2900fdd0506d70e9f77678e376cb65222f9a29
Crowdbotics <engineering@crowdbotics.com>
2023-07-07 12:16:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-haze-dev-98737",
+  url: "https://wispy-haze-dev-98737.botics.co",
+  api: "https://wispy-haze-dev-98737.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
